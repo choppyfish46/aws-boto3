5edb1184dec9e3df1f2229d76f379d9ea42a1f7f
choppyfish46 <choppyfish46@gmail.com>
2019-05-08 10:33:08 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+""" Boto3 is the Amazon Web Services (AWS) SDK for Python. It enables Python developers to create, configure, and manage AWS services, such as EC2 and S3. Boto3 provides an easy to use, object-oriented API, as well as low-level access to AWS services.
+
+pip3 install boto3 --user
+
+configure 
+
+aws configure
+
+test it 
+
+aws sts get-caller-identity
+
+list all s3 buckets 
+
+create a ec2 instance
+
+ """
